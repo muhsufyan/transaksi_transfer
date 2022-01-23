3e0f90938cf911abd71f6a832501300e9d7491fe
muhsufyan <muh.sufyan.ts@gmail.com>
2022-01-24 06:28:52 +0700

    karena id akun itu primary key jd tdk boleh diupdate sedangkan yg kita update hanya balance saja sehingga kita perlu menambahkan query agar id akun tdk terupdate caranya tambh NO KEY(account.sgl select * from accounts where id= limit 1 FOR NO KEY UPDATE)

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,7 +9,7 @@ INSERT INTO accounts (
 SELECT * FROM accounts WHERE id = $1 LIMIT 1;
 
 -- name: GetAccountForUpdate :one
-SELECT * FROM accounts WHERE id = $1 LIMIT 1 FOR UPDATE;
+SELECT * FROM accounts WHERE id = $1 LIMIT 1 FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts ORDER BY id LIMIT $1 OFFSET $2; 
